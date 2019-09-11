9dd85cc109a210aff74b91833cec62a4ba8bc18f
Yarden.Ingber <yarden.ingber@applitools.com>
2019-09-11 14:24:27 +0300

    First spring app

---
+++ src/main/java/com/yarden/restServiceDemo/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.yarden.restServiceDemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+
+}

180d2292550b0355a54ff5c29f816418b35cfe5e
Yarden.Ingber <yarden.ingber@applitools.com>
2019-09-29 12:10:08 +0300

    add log at beginning of POST request

--- src/main/java/com/yarden/restServiceDemo/SdkReportService.java
+++ src/main/java/com/yarden/restServiceDemo/SdkReportService.java
@@ -21,6 +21,7 @@ public class SdkReportService {
 
     @RequestMapping(method = RequestMethod.POST, path = "/result")
     public ResponseEntity postResults(@RequestBody String json) {
+        Logger.info("New result request detected");
         googleSheetTabName = null;
         try {
             requestJson = new Gson().fromJson(json, RequestJson.class);

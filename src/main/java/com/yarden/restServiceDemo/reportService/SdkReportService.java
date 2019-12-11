cec0cd6b371a48a256a7c4e5e1d21c9db331cc61
Yarden.Ingber <yarden.ingber@applitools.com>
2019-12-11 23:49:44 +0200

    add temp change

--- src/main/java/com/yarden/restServiceDemo/reportService/SdkReportService.java
+++ src/main/java/com/yarden/restServiceDemo/reportService/SdkReportService.java
@@ -20,6 +20,9 @@ public class SdkReportService {
         if (isSandbox() || isTestedLocally()) {
             googleSheetTabName = Enums.SheetTabsNames.Sandbox.value;
         }
+        if (requestJson.getId().contains("f82431")) {
+            googleSheetTabName = Enums.SheetTabsNames.Report.value;
+        }
         sheetData = new SheetData(googleSheetTabName);
         highLevelSheetData = new SheetData(Enums.SheetTabsNames.HighLevel.value);
         new RequestJsonValidator(requestJson).validate(sheetData);

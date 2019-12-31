3375f54baeb82647a4dac5236c1cd9f8f86f7cdd
Yarden.Ingber <yarden.ingber@applitools.com>
2019-12-31 18:29:48 +0200

    minor html

--- src/main/java/com/yarden/restServiceDemo/HtmlReportGenerator.java
+++ src/main/java/com/yarden/restServiceDemo/HtmlReportGenerator.java
@@ -73,8 +73,8 @@ public class HtmlReportGenerator {
         htmlReportStringBuilder.append("</head><body><div class=\"wrapper\">\n" +
                 "    <div class=\"content\">\n" +
                 "        <div class=\"header\">applitools</div>");
-        htmlReportStringBuilder.append("<h1>" + reportMailData.getReportTitle() + "</h1>");
-        htmlReportStringBuilder.append("<h2>Version</h2>");
+        htmlReportStringBuilder.append("<h2>" + reportMailData.getReportTitle() + "</h2>");
+        htmlReportStringBuilder.append("<h3>Version</h3>");
         htmlReportStringBuilder.append(reportMailData.getVersion() + "<br/><br/>");
         if (reportMailData.getChangeLog() != null && !reportMailData.getChangeLog().isEmpty()) {
             htmlReportStringBuilder.append("<details><summary><b>Change log</b></summary>");
@@ -82,11 +82,11 @@ public class HtmlReportGenerator {
             htmlReportStringBuilder.append("</details><br/>");
         }
         if (reportMailData.getHighLevelReportTable() != null) {
-            htmlReportStringBuilder.append("<h2>Test summary</h2><br/>");
+            htmlReportStringBuilder.append("<h3>Test summary</h3><br/>");
             htmlReportStringBuilder.append(reportMailData.getHighLevelReportTable());
         }
         if (reportMailData.getCoverageGap() != null && !reportMailData.getCoverageGap().isEmpty()) {
-            htmlReportStringBuilder.append("<br/><h2>Test coverage gap</h2>");
+            htmlReportStringBuilder.append("<br/><h3>Test coverage gap</h3>");
             htmlReportStringBuilder.append(reportMailData.getCoverageGap() + "<br/><br/>");
         }
         if (reportMailData.getDetailedMissingTestsTable() != null){

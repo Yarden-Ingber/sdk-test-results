d75ab098375284179ff8df7c06aaf15a9553496e
Yarden.Ingber <yarden.ingber@applitools.com>
2020-05-14 10:54:40 +0300

    fix mobile native tab name

--- src/main/java/com/yarden/restServiceDemo/Enums.java
+++ src/main/java/com/yarden/restServiceDemo/Enums.java
@@ -23,7 +23,7 @@ public class Enums {
     }
 
     public enum SdkGroupsSheetTabNames {
-        Selenium("Selenium"), Images("Images"), Appium("Appium"), Core("Core"), MobileNative("Mobile native");
+        Selenium("Selenium"), Images("Images"), Appium("Appium"), Core("Core"), MobileNative("MobileNative");
 
         public final String value;
 

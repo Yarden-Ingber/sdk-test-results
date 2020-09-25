7cb004f5970afc58c20cfadd4d54856784479c40
Yarden.Ingber <yarden.ingber@applitools.com>
2020-09-25 04:56:26 +0300

    add update ticket type request

--- src/main/java/com/yarden/restServiceDemo/kpis/TicketStates.java
+++ src/main/java/com/yarden/restServiceDemo/kpis/TicketStates.java
@@ -2,6 +2,6 @@ package com.yarden.restServiceDemo.kpis;
 
 public enum TicketStates {
 
-    New, StartedInvestigation, Reproduced, MissingInformation, WaitingForFieldInput, WorkInProgress, WaitingForFieldApproval, Done
+    New, StartedInvestigation, Reproduced, MissingInformation, WaitingForFieldInput, WorkInProgress, WaitingForFieldApproval, Done, NoState
 
 }

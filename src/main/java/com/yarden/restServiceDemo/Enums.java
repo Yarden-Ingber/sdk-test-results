b2870fa10dae718354a686da15f7237d0d724b2b
Yarden.Ingber <yarden.ingber@applitools.com>
2020-10-29 11:43:20 +0200

    fix env variable

--- src/main/java/com/yarden/restServiceDemo/Enums.java
+++ src/main/java/com/yarden/restServiceDemo/Enums.java
@@ -166,7 +166,7 @@ public class Enums {
         MailjetApiKeyPublic(System.getenv("MJ_APIKEY_PUBLIC")), MailjetApiKeyPrivate(System.getenv("MJ_APIKEY_PRIVATE")),
         AwsS3SdkReportsBucketName(System.getenv("SDK_REPORTS_S3_BUCKET")), AwsS3EyesReportsBucketName(System.getenv("EYE_REPORTS_S3_BUCKET")),
         MailReportRecipient(System.getenv("MAIL_REPORT_RECIPIENT")), SlackSdkReleaseChannelEndpoint(System.getenv("SDK_RELEASE_SLACK_CHANNEL_ENDPOINT")),
-        ApiToken(System.getenv("API_TOKEN")), TurnOffFullRegressionEmail("TURN_OFF_FULL_REGRESSION_EMAIL");
+        ApiToken(System.getenv("API_TOKEN")), TurnOffFullRegressionEmail(System.getenv("TURN_OFF_FULL_REGRESSION_EMAIL"));
 
         public final String value;
 

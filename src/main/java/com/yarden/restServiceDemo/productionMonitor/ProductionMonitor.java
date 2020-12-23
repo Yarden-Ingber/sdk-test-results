85d81254bc80ad33155a73966dd78251d31d6fc3
Yarden.Ingber <yarden.ingber@applitools.com>
2020-12-23 17:03:37 +0200

    add log

--- src/main/java/com/yarden/restServiceDemo/productionMonitor/ProductionMonitor.java
+++ src/main/java/com/yarden/restServiceDemo/productionMonitor/ProductionMonitor.java
@@ -69,6 +69,7 @@ public class ProductionMonitor extends TimerTask {
         String startTime = new SimpleDateFormat("MM/dd/yyyy:HH:mm:ss").format(calendar.getTime());
         String query = "search starttime=\"" + startTime + "\" endtime=\"" + endTime + "\" data.Info.RequestType=GetUserInfo OR data.Info.RequestType=StartSession OR data.Info.RequestType=MatchExpectedOutputAsSession | rex field=data.Context.RequestUrl \"(?<domain>https://.*\\.applitools.com)\" | stats count by domain data.Site | where NOT LIKE(domain, \"https://test%\") | rename data.Site as site | table domain site";
         String theString = new SplunkReporter().search(query, "csv", 1000);
+        new SplunkReporter().report(Enums.SplunkSourceTypes.ProductionMonitor, new JSONObject().put("eventType", "log").put("value", theString).toString());
         theString = theString.replace("\"", "").replace("domain,site\n", "");
         String[] domainsSitesList = theString.split("\n");
         StringBuilder failedEndpoints = new StringBuilder("");

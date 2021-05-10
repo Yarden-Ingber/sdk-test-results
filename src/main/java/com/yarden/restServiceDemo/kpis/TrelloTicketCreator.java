6c589e8c2cc8398049619139416371b4df2ed24a
Yarden.Ingber <yarden.ingber@applitools.com>
2021-05-10 22:47:40 +0300

    improvments

--- src/main/java/com/yarden/restServiceDemo/kpis/TrelloTicketCreator.java
+++ src/main/java/com/yarden/restServiceDemo/kpis/TrelloTicketCreator.java
@@ -5,26 +5,14 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URIBuilder;
 
 import java.net.URISyntaxException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class TrelloTicketCreator {
 
     public static void create(String formParams) throws URISyntaxException {
-        formParams = "http://www.website.com/head?" + formParams;
+        formParams = "http://www.website.com/dummyHead?" + formParams;
         List<NameValuePair> parameters = new URIBuilder(formParams).getQueryParams();
         Logger.info(parameters.toString());
-    }
-
-    private static Map urlParamsToMap(String urlParams) {
-        Map map = new HashMap();
-        String[] paramsList = urlParams.replace("+", " ").split("&");
-        for (String param : paramsList) {
-            String[] singleParamList = param.split("=");
-            map.put(singleParamList[0], singleParamList[1]);
-        }
-        return map;
     }
 
 }

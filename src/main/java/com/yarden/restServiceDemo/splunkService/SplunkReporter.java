021b68b7f142e0304df505203f611d4f20f755cb
Yarden.Ingber <yarden.ingber@applitools.com>
2021-06-29 18:31:44 +0300

    remove splunk from repo

--- src/main/java/com/yarden/restServiceDemo/splunkService/SplunkReporter.java
+++ src/main/java/com/yarden/restServiceDemo/splunkService/SplunkReporter.java
@@ -1,6 +1,6 @@
 package com.yarden.restServiceDemo.splunkService;
 
-import com.splunk.*;
+//import com.splunk.*;
 import com.yarden.restServiceDemo.Enums;
 import org.apache.commons.io.IOUtils;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -15,111 +15,111 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicReference;
 
-@Configuration
+//@Configuration
 public class SplunkReporter extends TimerTask {
 
-    private static Receiver receiver = null;
-    private static Service service = null;
-    private static AtomicReference<LinkedList<SplunkReportObject>> reportQueue = new AtomicReference<>();
-    private static final String lock = "lock";
-    private static boolean isRunning = false;
-    private static Timer timer;
+//    private static Receiver receiver = null;
+//    private static Service service = null;
+//    private static AtomicReference<LinkedList<SplunkReportObject>> reportQueue = new AtomicReference<>();
+//    private static final String lock = "lock";
+//    private static boolean isRunning = false;
+//    private static Timer timer;
 
     @EventListener(ApplicationReadyEvent.class)
     public static synchronized void start() {
-        if (!isRunning) {
-            timer = new Timer("SplunkReportQueue");
-            if (reportQueue.get() == null) {
-                reportQueue.set(new LinkedList<>());
-            }
-            timer.scheduleAtFixedRate(new SplunkReporter(), 30, 50);
-            isRunning = true;
-            System.out.println("SplunkReportQueue started");
-        }
+//        if (!isRunning) {
+//            timer = new Timer("SplunkReportQueue");
+//            if (reportQueue.get() == null) {
+//                reportQueue.set(new LinkedList<>());
+//            }
+//            timer.scheduleAtFixedRate(new SplunkReporter(), 30, 50);
+//            isRunning = true;
+//            System.out.println("SplunkReportQueue started");
+//        }
     }
 
     public void report(Enums.SplunkSourceTypes sourcetype, String json){
-        synchronized (lock) {
-            try {
-                reportQueue.get().add(new SplunkReportObject(sourcetype, json));
-            } catch (NullPointerException e) {
-            } catch (Throwable t) {
-                t.printStackTrace();
-            }
-        }
+//        synchronized (lock) {
+//            try {
+//                reportQueue.get().add(new SplunkReportObject(sourcetype, json));
+//            } catch (NullPointerException e) {
+//            } catch (Throwable t) {
+//                t.printStackTrace();
+//            }
+//        }
     }
 
     public String search(String query, String outputMode, int resultsCount) {
-        Job job = null;
-        try {
-            job = getService().getJobs().create(query);
-        } catch (Exception e) {
-            resetSplunkConnection();
-            job = getService().getJobs().create(query);
-        }
-        while (!job.isDone()) {
-            try {
-                Thread.sleep(500);
-            } catch (Throwable t) {}
-        }
-        CollectionArgs outputArgs = new CollectionArgs();
-        outputArgs.setCount(resultsCount);
-        outputArgs.put("output_mode", outputMode);
-        InputStream stream = job.getResults(outputArgs);
-        try {
-            return IOUtils.toString(stream, Charset.defaultCharset());
-        } catch (IOException e) {
+//        Job job = null;
+//        try {
+//            job = getService().getJobs().create(query);
+//        } catch (Exception e) {
+//            resetSplunkConnection();
+//            job = getService().getJobs().create(query);
+//        }
+//        while (!job.isDone()) {
+//            try {
+//                Thread.sleep(500);
+//            } catch (Throwable t) {}
+//        }
+//        CollectionArgs outputArgs = new CollectionArgs();
+//        outputArgs.setCount(resultsCount);
+//        outputArgs.put("output_mode", outputMode);
+//        InputStream stream = job.getResults(outputArgs);
+//        try {
+//            return IOUtils.toString(stream, Charset.defaultCharset());
+//        } catch (IOException e) {
             return "";
-        }
+//        }
     }
 
     @Override
     public void run() {
-        synchronized (lock) {
-            if (!reportQueue.get().isEmpty()) {
-                SplunkReportObject reportObject = reportQueue.get().removeFirst();
-                Args args = new Args();
-                args.add("sourcetype", reportObject.sourcetype.value);
-                try {
-                    getReceiver().log("qualityevents", args, reportObject.json);
-                } catch (Throwable t) {
-                    System.out.println("SplunkReporter: Retrying splunk log");
-                    try {
-                        resetSplunkConnection();
-                        getReceiver().log("qualityevents", args, reportObject.json);
-                    } catch (Throwable t2) {
-                        System.out.println("SplunkReporter: Failed logging to splunk: " + reportObject.json);
-                    }
-                }
-            }
-        }
+//        synchronized (lock) {
+//            if (!reportQueue.get().isEmpty()) {
+//                SplunkReportObject reportObject = reportQueue.get().removeFirst();
+//                Args args = new Args();
+//                args.add("sourcetype", reportObject.sourcetype.value);
+//                try {
+//                    getReceiver().log("qualityevents", args, reportObject.json);
+//                } catch (Throwable t) {
+//                    System.out.println("SplunkReporter: Retrying splunk log");
+//                    try {
+//                        resetSplunkConnection();
+//                        getReceiver().log("qualityevents", args, reportObject.json);
+//                    } catch (Throwable t2) {
+//                        System.out.println("SplunkReporter: Failed logging to splunk: " + reportObject.json);
+//                    }
+//                }
+//            }
+//        }
     }
 
-    private void resetSplunkConnection(){
-        receiver = null;
-        service = null;
-        System.gc();
-    }
-
-    private Service getService(){
-        if (service == null) {
-            ServiceArgs serviceArgs = new ServiceArgs();
-            serviceArgs.setHost("applitools.splunkcloud.com");
-            serviceArgs.setUsername(Enums.EnvVariables.SplunkUsername.value);
-            serviceArgs.setPassword(Enums.EnvVariables.SplunkPassword.value);
-            serviceArgs.setPort(8089);
-            serviceArgs.setSSLSecurityProtocol(SSLSecurityProtocol.TLSv1_2);
-            service = Service.connect(serviceArgs);
-        }
-        return service;
-    }
-
-    private Receiver getReceiver(){
-        if (receiver == null) {
-            return getService().getReceiver();
-        }
-        return receiver;
-    }
+//    private void resetSplunkConnection(){
+//        receiver = null;
+//        service = null;
+//        System.gc();
+//    }
+//
+//    private Service getService(){
+//        if (service == null) {
+//            ServiceArgs serviceArgs = new ServiceArgs();
+//            serviceArgs.setHost("applitools.splunkcloud.com");
+//            serviceArgs.setUsername(Enums.EnvVariables.SplunkUsername.value);
+//            serviceArgs.setPassword(Enums.EnvVariables.SplunkPassword.value);
+//            serviceArgs.setPort(8089);
+//            serviceArgs.setSSLSecurityProtocol(SSLSecurityProtocol.TLSv1_2);
+//            service = Service.connect(serviceArgs);
+//        }
+//        return service;
+//    }
+//
+//    private Receiver getReceiver(){
+//        if (receiver == null) {
+//            return getService().getReceiver();
+//        }
+//        return receiver;
+//    }
 
     public class SplunkReportObject {
         Enums.SplunkSourceTypes sourcetype;

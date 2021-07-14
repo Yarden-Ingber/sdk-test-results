24a60302328a3487a28d5ef597d73edc754c21b7
Yarden.Ingber <yarden.ingber@applitools.com>
2021-07-14 14:42:45 +0300

    Add fields to ticket description

--- src/main/java/com/yarden/restServiceDemo/kpis/TrelloTicketCreator.java
+++ src/main/java/com/yarden/restServiceDemo/kpis/TrelloTicketCreator.java
@@ -88,23 +88,29 @@ public class TrelloTicketCreator {
 
     private static void addTicketDetailsToDescription(ModelMap ticketFormFields) {
         String ticketDescription = (String)ticketFormFields.get(FormFields.ticketDescription.name());
-        if (!((String)ticketFormFields.get(FormFields.customerAppUrl.name())).isEmpty()) {
+        if (StringUtils.isNotEmpty((String)ticketFormFields.get(FormFields.customerAppUrl.name()))) {
             ticketDescription = ticketDescription + "\n\nCustomer app url: " + ticketFormFields.get(FormFields.customerAppUrl.name());
         }
-        if (!((String)ticketFormFields.get(FormFields.sdk.name())).isEmpty()) {
+        if (StringUtils.isNotEmpty((String)ticketFormFields.get(FormFields.sdk.name()))) {
             ticketDescription = ticketDescription + "\n\nSDK: " + ticketFormFields.get(FormFields.sdk.name());
         }
-        if (!((String)ticketFormFields.get(FormFields.sdkVersion.name())).isEmpty()) {
+        if (StringUtils.isNotEmpty((String)ticketFormFields.get(FormFields.sdkVersion.name()))) {
             ticketDescription = ticketDescription + "\n\nSDK version: " + ticketFormFields.get(FormFields.sdkVersion.name());
         }
-        if (!((String)ticketFormFields.get(FormFields.linkToTestResults.name())).isEmpty()) {
+        if (StringUtils.isNotEmpty((String)ticketFormFields.get(FormFields.linkToTestResults.name()))) {
             ticketDescription = ticketDescription + "\n\nEyes dashboard test results: " + ticketFormFields.get(FormFields.linkToTestResults.name());
         }
-        if (!((String)ticketFormFields.get(FormFields.isAppAccessible.name())).isEmpty()) {
+        if (StringUtils.isNotEmpty((String)ticketFormFields.get(FormFields.isAppAccessible.name()))) {
             ticketDescription = ticketDescription + "\n\nIs customer app accessible: " + ticketFormFields.get(FormFields.isAppAccessible.name());
         }
-        if (!((String)ticketFormFields.get(FormFields.renderID.name())).isEmpty()) {
+        if (StringUtils.isNotEmpty((String)ticketFormFields.get(FormFields.renderID.name()))) {
             ticketDescription = ticketDescription + "\n\nRender ID: " + ticketFormFields.get(FormFields.renderID.name());
+        }
+        if (StringUtils.isNotEmpty((String)ticketFormFields.get(FormFields.accountName.name()))) {
+            ticketDescription = ticketDescription + "\n\nCreated by: " + ticketFormFields.get(FormFields.accountName.name());
+        }
+        if (StringUtils.isNotEmpty((String)ticketFormFields.get(FormFields.zendeskCompanyName.name()))) {
+            ticketDescription = ticketDescription + "\n\nZendesk company name: " + ticketFormFields.get(FormFields.accountName.name());
         }
         ticketFormFields.addAttribute(TrelloTicketCreator.FormFields.ticketDescription.name(), ticketDescription);
     }
@@ -114,15 +120,15 @@ public class TrelloTicketCreator {
         fieldName = "KPI SUB PROJECT";
         fieldValue = (String)ticketFormFields.get(FormFields.sdk.name());
         TrelloApi.updateDropdownCustomFieldValue(ticketFormFields, fieldName, ticketId, fieldValue);
+        fieldName = "Created by";
+        fieldValue = (String)ticketFormFields.get(FormFields.accountName.name());
+        TrelloApi.updateStringCustomFieldValue(ticketFormFields, fieldName, ticketId, fieldValue);
         fieldName = "Affected Versions";
         fieldValue = (String)ticketFormFields.get(FormFields.sdkVersion.name());
         TrelloApi.updateStringCustomFieldValue(ticketFormFields, fieldName, ticketId, fieldValue);
         fieldName = "SDK";
         fieldValue = (String)ticketFormFields.get(FormFields.sdk.name());
         TrelloApi.updateDropdownCustomFieldValue(ticketFormFields, fieldName, ticketId, fieldValue);
-        fieldName = "Created by";
-        fieldValue = (String)ticketFormFields.get(FormFields.accountName.name());
-        TrelloApi.updateStringCustomFieldValue(ticketFormFields, fieldName, ticketId, fieldValue);
         fieldName = "Render ID";
         fieldValue = (String)ticketFormFields.get(FormFields.renderID.name());
         TrelloApi.updateStringCustomFieldValue(ticketFormFields, fieldName, ticketId, fieldValue);
